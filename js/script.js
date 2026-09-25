4ec07503abead8293461a3257db7accc9213bfc4
jtbf808 <jtbf@hawaii.edu>
2026-09-24 15:03:49 -1000

    Add style guide and logo, update layout and typography for improved design

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-console.log("script.js is loaded");
+/* MOBILE MENU */
+const siteNav = document.querySelector('.site-nav');
+const menuButton = document.querySelector('.menu-button');
+
+menuButton.onclick = () => {
+    if (siteNav.getAttribute('data-navstate') === 'open') {
+        siteNav.setAttribute('data-navstate', 'closed');
+    } else {
+        siteNav.setAttribute('data-navstate', 'open');
+    };
+}
